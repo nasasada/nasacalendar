cc77b66c4efe426ef96e02ef70eb420f1ee472fa
nasasada <sadaharuchallenge@gmail.com>
2025-09-19 05:12:46 +0900

    Update 01sano.js

--- js/01sano.js
+++ js/01sano.js
@@ -1,5 +1,6 @@
 // 01sano.js - 佐野さん専用設定
-calendarId = "sadaharuchallenge@gmail.com";   // ←佐野さんのカレンダーID
+calendarId = "jazzshogi81@gmail.com";   // ←佐野さんのカレンダーID
 apiKey     = "AIzaSyDCdmcXSeIaZTPnSJmNo5B8SdvjRakhF4A"; 
+
 
 
